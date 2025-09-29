832f6c01986854335a48c2550b3ac4e37523d7c3
simporechris <20468242@cegepoutaouais.qc.ca>
2025-09-29 10:46:39 -0400

    pas fini

---
+++ ex1.py
@@ -0,0 +1,14 @@
+ls_ex1 = [71, 55, 30, 42, 25, 68, 15]
+#1
+if ls_ex1 (2) %5:
+    print("Cet élément est divisible par 5.")
+else:
+    print("Cet élément n'est pas divisible par 5.")
+
+
+
+
+
+
+
+
