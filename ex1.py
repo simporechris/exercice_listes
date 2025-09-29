30b9f03084123f2d4df051400dba97b10ab8cab1
simporechris <2468242@cegepoutaouais.qc.ca>
2025-09-29 15:00:00 -0400

    pas fini

--- ex1.py
+++ ex1.py
@@ -1,10 +1,19 @@
 ls_ex1 = [71, 55, 30, 42, 25, 68, 15]
 #1
-if ls_ex1 (2) %5:
+if ls_ex1 [2] %5:
     print("Cet élément est divisible par 5.")
 else:
     print("Cet élément n'est pas divisible par 5.")
-
+#2
+if ls_ex1 [6] %2:
+    print("l'element est pair")
+else:
+    print("l'element n'est pas pair")
+#3
+if ls_ex1 [1] %2:
+    print("l'element est pair")
+else:
+    print("l'element n'est pas pair")
 
 
 
